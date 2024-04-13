7919b1c541dd8983f5b3e6bd6341896d89fac20f
tomyjany <tomas.janovec@tul.cz>
2024-04-13 19:18:13 +0200

    logger

--- src/main/java/com/inn/weatherApp/WeatherAppApplication.java
+++ src/main/java/com/inn/weatherApp/WeatherAppApplication.java
@@ -3,12 +3,16 @@ package com.inn.weatherApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;1
 
 @SpringBootApplication
 public class WeatherAppApplication {
+	private static final Logger logger = LoggerFactory.getLogger(WeatherAppApplication.class);
 
 
 	public static void main(String[] args) {
+		logger.info("Database URL: {}", System.getenv("SPRING_DATASOURCE_URL"));
 		SpringApplication.run(WeatherAppApplication.class, args);
 	}
 
