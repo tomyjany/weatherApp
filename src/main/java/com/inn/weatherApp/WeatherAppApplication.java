e2571be82b5e5853b3614e10bc30b23f46a5bbf1
tomyjany <tomas.janovec@tul.cz>
2024-04-13 21:47:07 +0200

    change packaging

--- src/main/java/com/inn/weatherApp/WeatherAppApplication.java
+++ src/main/java/com/inn/weatherApp/WeatherAppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+/*
 @SpringBootApplication
 public class WeatherAppApplication {
 	private static final Logger logger = LoggerFactory.getLogger(WeatherAppApplication.class);
@@ -17,3 +17,22 @@ public class WeatherAppApplication {
 	}
 
 }
+*/
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class WeatherAppApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WeatherAppApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherAppApplication.class, args);
+	}
+}
+
