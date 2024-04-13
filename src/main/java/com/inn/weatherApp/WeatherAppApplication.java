17726ede65059c8cbf078446dfce0e99de749664
tomyjany <tomas.janovec@tul.cz>
2024-04-13 23:03:02 +0200

    fixed issue with imports

--- src/main/java/com/inn/weatherApp/WeatherAppApplication.java
+++ src/main/java/com/inn/weatherApp/WeatherAppApplication.java
@@ -16,12 +16,12 @@ public class WeatherAppApplication {
 	}
 
 }
+/*
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-/*
 @SpringBootApplication
 public class WeatherAppApplication extends SpringBootServletInitializer {
 
