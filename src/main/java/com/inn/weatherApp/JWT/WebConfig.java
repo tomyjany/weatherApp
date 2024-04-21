b4f88c593ad1759bdbf68daf0cf949a559dcc11d
tomyjany <tomas.janovec@tul.cz>
2024-04-21 22:38:47 +0200

    repair

--- src/main/java/com/inn/weatherApp/JWT/WebConfig.java
+++ src/main/java/com/inn/weatherApp/JWT/WebConfig.java
@@ -15,6 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("https://pocasko.azurewebsites.net")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
