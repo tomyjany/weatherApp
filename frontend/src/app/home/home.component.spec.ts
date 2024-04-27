accd9fd52c70db6d869fcb2f277e3a2b8b892871
Tomas Janovec <74786110+tomyjany@users.noreply.github.com>
2024-04-27 11:44:53 +0200

    Tests frontend (#25)
    * Write some tests
    
    * edit github workflow
    
    * edit workflow
    
    * add more tests for coverage

--- frontend/src/app/home/home.component.spec.ts
+++ frontend/src/app/home/home.component.spec.ts
@@ -1,23 +1,54 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomeComponent } from './home.component';
+import { AuthService } from '../auth.service';
+
+// Define a mock class for AuthService
+class MockAuthService {
+  isLoggedIn = jest.fn().mockReturnValue(false);
+  getUserEmail = jest.fn().mockReturnValue(null);
+  isSubscribed = jest.fn().mockReturnValue(false);
+  logout = jest.fn();
+}
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
-
+  let mockAuthService:MockAuthService;
+  let authService:AuthService;
+  //let authService:Partial<AuthService>;
   beforeEach(async () => {
+    mockAuthService = new MockAuthService();
     await TestBed.configureTestingModule({
-      declarations: [HomeComponent]
-    })
-    .compileComponents();
-    
+      declarations: [HomeComponent],
+      providers: [{ provide: AuthService, useValue: mockAuthService }],
+    }).compileComponents();
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    authService = TestBed.inject(AuthService); 
   });
-
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-});
+  
+  it('should update user status on initialization', () => {
+    (authService.isLoggedIn as jest.Mock).mockReturnValue(true);
+    (authService.getUserEmail as jest.Mock).mockReturnValue('test@example.com');
+    (authService.isSubscribed as jest.Mock).mockReturnValue(true);
+  
+    component.ngOnInit();
+  
+    expect(component.isLoggedIn).toBe(true);
+    expect(component.userEmail).toBe('test@example.com');
+    expect(component.isSubscribed).toBe(true);
+  });
+  
+  it('should call logout and update user status', () => {
+    component.logout();
+  
+    expect(authService.logout).toHaveBeenCalled();
+    expect(component.isLoggedIn).toBe(false);
+    expect(component.userEmail).toBeNull();
+    expect(component.isSubscribed).toBe(false);
+  });
+  
+})
